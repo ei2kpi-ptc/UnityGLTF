56032f911c6b8d94236b4752ce98180c94c8b6ba
Varun Mani <44373229+ei2kpi-ptc@users.noreply.github.com>
2019-05-07 14:27:16 -0400

    Update get-git-tag

--- scripts/get-git-tag.ps1
+++ scripts/get-git-tag.ps1
@@ -4,4 +4,9 @@ $isTag = [regex]::IsMatch($Env:BUILD_SOURCEBRANCH, "^(refs\/tags\/)")
 if($isTag)
 {
     $Env:GIT_TAG = $Env:BUILD_SOURCEBRANCH.Split("/refs/tags")[-1]
+	Write-Host "This is a Tagged release so setting GIT_TAG env Variable to: $Env:GIT_TAG"
+}
+else
+{
+	Write-Host "Not a tagged release so not setting Env:GIT_TAG variable"
 }
