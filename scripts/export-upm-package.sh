7f9f636cb7db92f62a81a68c3e9fdcd469c89a10
Varun Mani <vmani@ptc.com>
2019-05-07 15:58:51 -0400

    Combined get-git-tag with Export-upm-package and removed zipping steps

--- scripts/export-upm-package.sh
+++ scripts/export-upm-package.sh
@@ -12,6 +12,17 @@ upm_staging_UWP_plugins_path=$upm_staging_path/UnityGLTF/Plugins/uap10.0.10586
 upm_zip_export_path=$(pwd)/current-package/$upm_name.zip
 upm_targz_export_path=$(pwd)/current-package/$upm_name.tar.gz
 
+if [[ $BUILD_SOURCEBRANCH == *"refs/tags"* ]]; then
+  echo "Detected refs/tags in $BUILD_SOURCEBRANCH so this must be a tagged release build."
+  # Splits the string with "refs/tags", takes the second value and then 
+  # swaps out any slashes for underscores
+  GIT_TAG=$(echo $BUILD_SOURCEBRANCH | awk -F'refs/tags/' '{print $2}' | tr '/' '_')
+  echo "Setting GIT_TAG variable to: $GIT_TAG"
+  echo "##vso[task.setvariable variable=GIT_TAG]$GIT_TAG"
+else
+  echo "Did not detect refs/tags in $BUILD_SOURCEBRANCH so skipping GIT_TAG variable set"
+fi
+
 # msbuild spits out every single dependency dll for UWP
 # These are the only files that are needed by the UPM package for Unity 2018.3+
 # Including all the files in the UWP plugin directory causes name collision errors when 
@@ -49,18 +60,3 @@ for entry in "$upm_staging_UWP_plugins_path"/*
 do
   echo "$entry"
 done
-
-echo "Installing zip package"
-sudo apt-get install zip
-
-echo "Creating .zip of UPM package"
-sudo zip -q -r $upm_zip_export_path ./
-
-echo "Creating .tar.gz of UPM package"
-tar -zcf $upm_targz_export_path ./
-
-echo "Changing back to original folder $cached_folder"
-cd $cached_folder
-
-echo "Finishing with code $error_code"
-exit $error_code
